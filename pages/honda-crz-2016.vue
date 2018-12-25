2b09d5317d0f96df06445ee152e5624645f2b1bc
Vitaly Kezlya <vitaly@kezlya.com>
2018-12-25 15:20:23 +0300

    Testing product page

---
+++ pages/honda-crz-2016.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <section>
+      <v-layout
+        column
+        wrap
+        class="my-5"
+        align-center
+      >
+        <v-flex xs12 sm4 class="my-3">
+          <div class="text-xs-center">
+            <h2 class="headline">Honda CRZ 2016</h2>
+            <span class="subheading">
+              4K miles
+            </span>
+          </div>
+        </v-flex>
+
+      </v-layout>
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data: () => ({
+      feedback: "",
+    }),
+  }
+</script>
