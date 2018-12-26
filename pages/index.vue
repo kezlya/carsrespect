7c048d6ab98f49953c452b458f647dcf05acc568
Vitaly Kezlya <vitaly@kezlya.com>
2018-12-26 15:52:21 +0300

    For sale section

--- pages/index.vue
+++ pages/index.vue
@@ -28,8 +28,9 @@
       <v-layout column wrap class="py-5" align-center white>
         <v-flex xs12 sm4 class="my-3">
           <div class="text-xs-center">
+            <a id="services"></a>
             <h2 class="display-2 py-2">Services</h2>
-            <span class="subheading"><a id="services"></a>
+            <span class="subheading">
               We service all makes and models. Domestic and Foreign.
             </span>
           </div>
@@ -61,186 +62,43 @@
 
     <section>
       <v-container fill-height pa-5 >
-
-        <v-layout
-          row
-          wrap
-          class="pa-2"
-          align-center
-          >
+        <v-layout row wrap class="pa-2" align-center>
           <v-flex xs12 pb-5 ml-2>
+            <a id="sale"></a>
             <div class="text-xs-center">
-              <h2 class="headline py-2"><a id="sale"></a>For sale</h2>
-          </div>
+              <h2 class="display-2 py-2">For sale</h2>
+              <span class="subheading">
+                All cars are fully inspected! One year warranty on mechanical parts.
+                <a href="#contact">Contact us</a> to pre order vehicle that you always wanted.
+              </span>
+            </div>
           </v-flex>
+          <template v-for="car in cars">
+            <v-flex xs12 md6 lg4 pa-2>
+              <v-card>
+                <v-img height="200px" :src="'cars/'+car.name+'/1.jpg'">
+                  <v-container fill-height fluid>
+                    <v-layout fill-height>
+                      <v-flex xs12 align-end flexbox>
+                        <v-chip class="subheading red--text white elevation-2"><strike>{{car.oldPrice}}</strike>  </v-chip>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-img>
+                <v-card-title>
+                  <div>
+                    <span class="title">{{car.title}}</span> <v-spacer/><span>{{car.newPrice}}</span><br>
+                    <span class="grey--text">{{car.desc}}</span><br>
+                  </div>
+                </v-card-title>
+                <v-card-actions>
+                  <v-btn flat color="indigo">More details</v-btn>
+                  <v-btn flat href="#contact" color="indigo">Test drive</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+          </template>
 
-          <v-flex xs12 md6 lg4 pa-2>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                src="IMG_4168.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <v-chip class="title red--text white elevation-2"><strike>15.000</strike>  </v-chip>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
-              <v-card-title>
-                <div>
-                  <span>Honda CRZ, 2016</span> <v-spacer/><span>$9.999</span><br>
-                  <span class="grey--text">Manual transmision .asdfas .a sdfa. asdfa.s </span><br>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">More details</v-btn>
-                <v-btn flat color="orange">Test drive</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-
-          <v-flex xs12 md6 lg4 pa-2>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                src="IMG_4168.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <v-chip class="title red--text white elevation-2"><strike>15.000</strike>  </v-chip>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
-              <v-card-title>
-                <div>
-                  <span>Honda CRZ, 2016</span> <v-spacer/><span>$9.999</span><br>
-                  <span class="grey--text">Manual transmision .asdfas .a sdfa. asdfa.s </span><br>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">More details</v-btn>
-                <v-btn flat color="orange">Test drive</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-
-          <v-flex xs12 md6 lg4 pa-2>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                src="IMG_4168.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <v-chip class="title red--text white elevation-2"><strike>15.000</strike>  </v-chip>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
-              <v-card-title>
-                <div>
-                  <span>Honda CRZ, 2016</span> <v-spacer/><span>$9.999</span><br>
-                  <span class="grey--text">Manual transmision .asdfas .a sdfa. asdfa.s </span><br>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">More details</v-btn>
-                <v-btn flat color="orange">Test drive</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-
-          <v-flex xs12 md6 lg4 pa-2>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                src="IMG_4168.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <v-chip class="title red--text white elevation-2"><strike>15.000</strike>  </v-chip>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
-              <v-card-title>
-                <div>
-                  <span>Honda CRZ, 2016</span> <v-spacer/><span>$9.999</span><br>
-                  <span class="grey--text">Manual transmision .asdfas .a sdfa. asdfa.s </span><br>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">More details</v-btn>
-                <v-btn flat color="orange">Test drive</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-
-          <v-flex xs12 md6 lg4 pa-2>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                src="IMG_4168.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <v-chip class="title red--text white elevation-2"><strike>15.000</strike>  </v-chip>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
-              <v-card-title>
-                <div>
-                  <span>Honda CRZ, 2016</span> <v-spacer/><span>$9.999</span><br>
-                  <span class="grey--text">Manual transmision .asdfas .a sdfa. asdfa.s </span><br>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">More details</v-btn>
-                <v-btn flat color="orange">Test drive</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-
-          <v-flex xs12 md6 lg4 pa-2>
-            <v-card>
-              <v-img
-                class="white--text"
-                height="200px"
-                src="IMG_4168.jpg"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <v-chip class="title red--text white elevation-2"><strike>15.000</strike>  </v-chip>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-img>
-              <v-card-title>
-                <div>
-                  <span>Honda CRZ, 2016</span> <v-spacer/><span>$9.999</span><br>
-                  <span class="grey--text">Manual transmision .asdfas .a sdfa. asdfa.s </span><br>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat color="orange">More details</v-btn>
-                <v-btn flat color="orange">Test drive</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
 
         </v-layout>
       </v-container>
@@ -350,7 +208,7 @@
     </section>
 
     <section>
-<a id="contact"></a>
+      <a id="contact"></a>
         <v-layout row wrap justify-center align-center white class="my-0 pa-0">
           <v-flex xs12 sm8>
             <iframe width="100%" src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d48957.93166082604!2d-75.23429345152195!3d39.921909032951184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x89c6b7d8d4b54beb%3A0x89f514d88c3e58c1!2sPhiladelphia%2C+Pennsylvania%2C+USA!3m2!1d39.9525839!2d-75.1652215!4m5!1s0x89c6c3f2d28dcdfb%3A0x352e216800820a8c!2s522+Ellis+Ave+Unit+1-C%2C+Darby%2C+PA+19023%2C+USA!3m2!1d39.9112115!2d-75.2572854!5e0!3m2!1sen!2sru!4v1545740728637" height="450px" frameborder="0" style="border:0" allowfullscreen></iframe>
@@ -412,6 +270,15 @@ export default {
         "Electrical service",
         "Break change",
         "Inspection",
+      ],
+      cars:[
+        {
+          name: "mazda",
+          title:"2012 Mazda Miata MX5 17K mil",
+          oldPrice: "$15.000",
+          newPrice: "$9.500",
+          desc: "Grand Touring; CONVERTIBLE 2-DR; 2.0L L4 DOHC 16V; 6-Speed Manual; RWD"
+        }
       ],
     }),
   }
